18ae843dde2f6be0d4f5a9fb5ac677c6ca6479e8
AsilbekAsqarov17 <asilbek.asqarov.2023@gmail.com>
2026-03-19 17:17:58 +0500

    4 dars uy vazifasi

---
+++ dars4.py
@@ -0,0 +1,22 @@
+#Asilbek Asqarov
+#Mohirdev python-darslari 
+#dars-4
+
+#task1
+salom_dunyo = "Hello World!"
+print(salom_dunyo)
+
+#task2
+xabar = "3 uy vazifa bajarilyapti"
+print(xabar)
+xabar = "3 uy vazifa bajarildi"
+print(xabar)
+
+#task3
+#class can not be variable
+
+#task4
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
