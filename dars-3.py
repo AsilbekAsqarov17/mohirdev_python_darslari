283c33d9c769115af7123b1b255eac04767a39d9
AsilbekAsqarov17 <asilbek.asqarov.2023@gmail.com>
2026-03-19 17:09:13 +0500

    3 dars uy vazifasi

---
+++ dars-3.py
@@ -0,0 +1,25 @@
+#Asilbek Asqarov
+#Mohirdev python-darslari 
+#dars-3
+
+#task1
+print('"Nexia", "Tico", \'Damas\' ko\'rganlar qilar havas')
+#task2
+print(5**4)
+#task3
+print(22%4)
+#task4
+tomon = 125
+print("Tomonlari 125 ga teng kvadratning yuzi:",tomon*tomon)
+
+print("Tomonlari 125 ga teng kvadratning perimetr: ", (tomon)*2)
+
+#task5
+diametr = 12
+PI =3.14
+print("Diametri 12 ga teng bo'lgan doiraning yuzi: ",PI *(diametr/2)**2)
+
+#task6
+katet1 = 6
+katet2 = 7
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi: ", (katet1**2 + katet2**2)**(1/2))
