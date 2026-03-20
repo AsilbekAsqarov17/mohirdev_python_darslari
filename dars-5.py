74401fed0cc5b910cc434fa9d42fc865b53d7858
AsilbekAsqarov17 <asilbek.asqarov.2023@gmail.com>
2026-03-20 08:31:59 +0500

    5 dars uy vazifasi

---
+++ dars-5.py
@@ -0,0 +1,30 @@
+#Asilbek Asqarov
+#Mohirdev python-darslari 
+#dars-5
+
+#task1
+kocha="Bog'bon"
+mahalla="Sog'bon"
+tuman="Bodomzor" 
+viloyat="Samarqand"
+
+#task2
+print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+
+#task3
+kocha = input("Ko'cha nomi: ")
+mahalla = input("Mahalla nomi: ")
+tuman = input("Tuman nomi")
+viloyat =input("Viloyat nomi: ")
+print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+
+print(f"{kocha} ko'chasi,\n {mahalla} mahallasi,\n {tuman} tumani,\n {viloyat} viloyati")
+
+
+#task3
+manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+
+print(manzil.title())
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.capitalize())
